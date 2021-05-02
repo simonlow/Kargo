eb19c4824e24b3b838ce4253905cd0d260ae212d
Simon Low <simonlow@Simons-MacBook-Pro.local>
2021-05-02 19:55:16 -0400

    added newly named main.js

---
+++ main.test.js
@@ -0,0 +1,16 @@
+const { test, expect } = require("@jest/globals")
+const stringArray = require("./main")
+
+test('properly changes array to String', () => {
+    let arr = [1,1];
+    expect(stringArray(arr)).toBe("One,One");
+})
+test('different single digit numbers', () => {
+    expect(stringArray([1,2,3])).toBe("One,Two,Three");
+})
+test('longer array with multi-digit numbers', () => {
+    expect(stringArray([3, 35, 209])).toBe("Three,ThreeFive,TwoZeroNine");
+})
+test('work example', () => {
+    expect(stringArray([10, 300, 5])).toBe("OneZero,ThreeZeroZero,Five");
+})
