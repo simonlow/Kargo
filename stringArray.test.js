a586979db90f1cb956f447f7df6d942f58e16029
Simon Low <simonlow@Simons-MacBook-Pro.local>
2021-05-02 19:25:44 -0400

    Changes were made to stringArray.test, must commit before merging with main

--- stringArray.test.js
+++ stringArray.test.js
@@ -1,6 +1,16 @@
-const { test } = require("@jest/globals")
-const arr = require("./stringArray")
+const { test, expect } = require("@jest/globals")
+const stringArray = require("./stringArray")
 
 test('properly changes array to String', () => {
-    expect(stringArray([1, 1])).toBe([One, One]);
+    let arr = [1,1];
+    expect(stringArray(arr)).toBe("One,One");
+})
+test('different single digit numbers', () => {
+    expect(stringArray([1,2,3])).toBe("One,Two,Three");
+})
+test('longer array with multi-digit numbers', () => {
+    expect(stringArray([3, 35, 209])).toBe("Three,ThreeFive,TwoZeroNine");
+})
+test('work example', () => {
+    expect(stringArray([10, 300, 5])).toBe("OneZero,ThreeZeroZero,Five");
 })
